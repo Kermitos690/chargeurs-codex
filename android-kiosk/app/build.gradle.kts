20fedf821a4a4b118bb1532521b2a2e07f710e2d
Kermitos690 <91438714+Kermitos690@users.noreply.github.com>
2026-07-25 10:10:14 +0200

    Bump DTA call graph analyzer to v1.4.0

--- android-kiosk/app/build.gradle.kts
+++ android-kiosk/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "ch.chargeurs.kiosk"
         minSdk = 26
         targetSdk = 36
-        versionCode = 130
-        versionName = "1.3.0"
+        versionCode = 140
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
         buildConfigField("String", "ENROLLMENT_URL", quotedBuildConfig(enrollmentUrl.get()))
