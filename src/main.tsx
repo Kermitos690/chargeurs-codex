7590d2e9d5369f884d0b8496467d53206245266c
Kermitos690 <91438714+Kermitos690@users.noreply.github.com>
2026-08-10 04:38:33 +0200

    feat(kiosk): load V2 overlay layout rules

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import "./kiosk-field.css";
 import "./kiosk-customer-polish.css";
 import "./kiosk-v2.css";
+import "./kiosk-v2-overlays.css";
 import { KioskBlankScreenGuard, KioskErrorBoundary } from "./components/kiosk/KioskRuntimeGuard";
 import { initKioskPwa } from "./pwa/registerSW";
 
