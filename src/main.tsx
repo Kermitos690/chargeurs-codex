cdbde2c3114dd277a6e87f69b049fd9b379ac00d
Kermitos690 <91438714+Kermitos690@users.noreply.github.com>
2026-08-10 04:36:12 +0200

    feat(kiosk): load V2 landscape retail styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from "./App.tsx";
 import "./index.css";
 import "./kiosk-field.css";
 import "./kiosk-customer-polish.css";
+import "./kiosk-v2.css";
 import { KioskBlankScreenGuard, KioskErrorBoundary } from "./components/kiosk/KioskRuntimeGuard";
 import { initKioskPwa } from "./pwa/registerSW";
 
