fb2e6c734e4d355bd448c11107d210b4677ecb77
Kermitos690 <91438714+Kermitos690@users.noreply.github.com>
2026-08-27 02:59:34 +0200

    feat(volt): mount assistant across customer pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import { KioskOffersLauncher } from "./components/kiosk/KioskOffersLauncher.tsx"
 import { KioskOperationalGuard } from "./components/kiosk/KioskOperationalGuard.tsx";
 import { KioskNativeIdleUpdateGuard } from "./components/kiosk/KioskNativeIdleUpdateGuard.tsx";
 import { KioskIdentityGate } from "./components/kiosk/KioskIdentityGate.tsx";
+import { VoltWidget } from "./components/support/VoltWidget.tsx";
 
 import CityPowerbank from "./pages/CityPowerbank.tsx";
 import Partners from "./pages/Partners.tsx";
@@ -75,6 +76,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <Router>
+          <VoltWidget />
           <KioskReturnOverlay />
           <KioskHelpLauncher />
           <KioskOffersLauncher />
